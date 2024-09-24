096eeefc5727b8dde017a9011771ddd12c69f5eb
Juan Carlos <gurquiza8@gmail.com>
2024-09-24 11:41:23 -0600

    feature: separating in components all the layout

--- src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
+++ src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
@@ -1,14 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from "../../../shared/components/side-menu/side-menu.component";
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SideMenuComponent
+  ],
   templateUrl: './admin-layout.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class AdminLayoutComponent {
+
+  isAuthenticated = signal(false);
+
+  public onLogin() {
+    this.isAuthenticated.set(true);
+  }
+
+  public onLogout() {
+    this.isAuthenticated.set(false);
+  }
 
 }
---
+++ src/app/modules/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-side-menu',
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+  ],
+  templateUrl: './side-menu.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SideMenuComponent {
+
+  isAuthenticated = input(false);
+
+  onSignOut = output();
+  onSignIn = output();
+
+}
